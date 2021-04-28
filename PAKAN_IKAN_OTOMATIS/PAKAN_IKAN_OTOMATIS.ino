f882d266d20f47305f1b1a5f14873b558b71b07a
emrifan6 <39058887+emrifan6@users.noreply.github.com>
2021-04-28 15:19:25 +0700

    Update PAKAN_IKAN_OTOMATIS.ino

--- PAKAN_IKAN_OTOMATIS/PAKAN_IKAN_OTOMATIS.ino
+++ PAKAN_IKAN_OTOMATIS/PAKAN_IKAN_OTOMATIS.ino
@@ -4,7 +4,7 @@
 Servo myservo;  // create servo object to control a servo 
 DS3231  rtc(SDA, SCL);
 Time  t;
-#define buz 11
+#define buzzer 4
 int Hor;
 int Min;
 int Sec;
@@ -14,24 +14,30 @@ int MotorPin = 10;
 int JamPakanPagi = 6;
 int MenitPakanPagi = 0;
 
-int JamPakanSore = 17;
-int MenitPakanSore = 0;
+int JamPakanSiang = 12;
+int MenitPakanSiang = 0;
+
+int JamPakanSore = 15;
+int MenitPakanSore = 18;
 
 
 void setup() {
   analogWrite(MotorPin, LOW);
-  myservo.attach(11);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   myservo.write(0);
   Wire.begin();
   rtc.begin();
   Serial.begin(9600);
-  // The following lines can be uncommented to set the date and time
-  //rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
-//  rtc.setTime(22, 41, 10);     // Set the time to 12:00:00 (24hr format)
-//  rtc.setDate(5, 2, 2021);   // Set the date to January 1st, 2014
+//   The following lines can be uncommented to set the date and time
+//  rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
+//  rtc.setTime(17, 00, 00);     // Set the time to 12:00:00 (24hr format)
+//  rtc.setDate(27, 4, 2021);   // Set the date to January 1st, 2014
   delay(1000);
   Serial.print("WAKTU PEMBERIAN PAKAN PAGI : ");
   Serial.print(JamPakanPagi);
+  Serial.print(":");
+  Serial.print("WAKTU PEMBERIAN PAKAN SIANG : ");
+  Serial.print(JamPakanSiang);
   Serial.print(":");
   Serial.println(MenitPakanPagi);
   Serial.print("WAKTU PEMBERIAN PAKAN SORE : ");
@@ -54,6 +60,9 @@ void loop() {
   if ( Hor == JamPakanPagi &&  Min == MenitPakanPagi) {
     Serial.println("WAKTU PEMBERIAN PAKAN PAGI");
     Beri_pakan();
+  }else if( Hor == JamPakanSiang &&  Min == MenitPakanSiang){
+    Serial.println("WAKTU PEMBERIAN PAKAN SIANG");
+    Beri_pakan();
   }else if( Hor == JamPakanSore &&  Min == MenitPakanSore){
     Serial.println("WAKTU PEMBERIAN PAKAN SORE");
     Beri_pakan();
@@ -73,4 +82,21 @@ void Beri_pakan(){
   delay(300);
   myservo.write(0);
   analogWrite(MotorPin, LOW);
+}
+
+void buzz() {
+  digitalWrite(buzzer, HIGH);
+  delay(50);
+  digitalWrite(buzzer, LOW);
+  delay(50);
+  digitalWrite(buzzer, HIGH);
+  delay(50);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
+  delay(50);
+  digitalWrite(buzzer, HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
 }
